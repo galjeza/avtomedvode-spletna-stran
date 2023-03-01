0139bbb6246d137c86207aeb2e7a15ac320b8b2c
galjeza <gal.jeza@protonmail.com>
2023-03-01 10:39:08 +0100

    rm asynch

--- src/pages/odkup/Form.tsx
+++ src/pages/odkup/Form.tsx
@@ -45,7 +45,7 @@ export default function Example() {
 
 
 
-const handleSubmit = async (e: { preventDefault: () => void; }) => {
+const handleSubmit =  (e: { preventDefault: () => void; }) => {
     e.preventDefault();
     const data = {
       name,
@@ -491,7 +491,8 @@ const handleSubmit = async (e: { preventDefault: () => void; }) => {
             <div className="sm:col-span-2">
               {formSubmitted? <div className="text-center text-green-500 text-2xl font-bold">Obrazec je uspešno oddan!</div> : <button
                 type="submit"
-                onClick={handleSubmit}
+                onClick={handleSubmit
+                }
                 className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
               >
                 Pošlji
